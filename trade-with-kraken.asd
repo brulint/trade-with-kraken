9b6567095ad087e0a69d00962eafd3c1719e45d6
carboleum <66366730+carboleum@users.noreply.github.com>
2020-08-10 13:31:19 +0200

    Add files via upload

---
+++ trade-with-kraken.asd
@@ -0,0 +1,3 @@
+(defsystem :trade-with-kraken
+    :depends-on (:cl-kraken)
+    :components ((:file "trade-with-kraken")))
---
+++ trade-with-kraken.lisp
@@ -0,0 +1,31 @@
+
+(defpackage :trade-with-kraken
+  (:use :cl)
+  (:export :get-public
+           :post-private)
+  )
+
+(in-package :trade-with-kraken)
+
+;; todo: check file exists                                              
+(with-open-file (stream "~/lisp/kraken.key")
+  (defparameter *api-key* (read-line stream nil))
+  (defparameter *api-secret* (read-line stream nil)))
+
+(defun signature (method &optional parameters)
+  (let* ((nonce (cl-kraken/src/time:generate-kraken-nonce))
+         (path (concatenate 'string "/0/private/" method))
+         (uri (concatenate 'string "https://api.kraken.com" path))
+         (parameters (acons "nonce" nonce parameters))
+         (signature (cl-kraken/src/cryptography:signature path nonce parameters *api-secret*))
+         (headers `(("api-key" . ,*api-key*) ("api-sign" . ,signature))))
+    (values uri parameters headers)))
+
+(defun get-public (method &optional parameters)
+  (let ((uri (concatenate 'string "https://api.kraken.com/0/public/" method)))
+    (drakma:http-request uri :parameters parameters)))
+
+(defun post-private (method &optional parameters)
+  (multiple-value-bind (uri parameters headers) (signature method parameters)
+    (drakma:http-request uri :method :post :parameters parameters :additional-headers headers)))
+
